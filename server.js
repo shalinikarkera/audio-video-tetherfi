dfd5584365191143a733e745ac2c26b1dbf8f2a9
shalini <shalini.karkera8@gmail.com>
2023-11-26 19:23:55 +0530

    deplotment changes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
-const path = require("path");
 app.set("view engine", "ejs");
 const io = require("socket.io")(server, {
   cors: {
@@ -17,19 +16,6 @@ const opinions = {
 app.use("/peerjs", ExpressPeerServer(server, opinions));
 app.use(express.static("public"));
 
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "room.ejs")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "room.ejs"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
